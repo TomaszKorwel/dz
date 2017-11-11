e80949dee5dd3e333c92a6f37688f48a384c262b
Vadim Tkachenko <vt@seriousinsights.com>
2017-11-10 22:18:51 -0700

    Added instrumentation to compare json/protobuf
    Quick take: protobuf generates message that is three times shorter,
    but takes ten times longer to do that

--- dz3-master/dz3-http/src/test/java/net/sf/dz3/view/http/v2/JsonRendererTest.java
+++ dz3-master/dz3-http/src/test/java/net/sf/dz3/view/http/v2/JsonRendererTest.java
@@ -11,9 +11,12 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.NDC;
 import org.junit.Test;
 
+import com.google.gson.Gson;
+
 import net.sf.dz3.device.model.ThermostatSignal;
 import net.sf.dz3.device.model.impl.ThermostatModel;
 import net.sf.dz3.device.sensor.impl.NullSensor;
+import net.sf.dz3.instrumentation.Marker;
 import net.sf.dz3.view.http.common.QueueFeeder;
 import net.sf.jukebox.datastream.signal.model.DataSample;
 
@@ -28,7 +31,7 @@ public class JsonRendererTest {
 
         try {
 
-            ThermostatModel t = new ThermostatModel("json-thermostat", new NullSensor("null-address", 100), 20, 1, 0.000002,0, 2);
+            ThermostatModel t = new ThermostatModel("test-thermostat", new NullSensor("null-address", 100), 20, 1, 0.000002,0, 2);
             Map<String, Object> context = new HashMap<String, Object>();
             BlockingQueue<ZoneSnapshot> queue = new LinkedBlockingQueue<ZoneSnapshot>();
 
@@ -39,13 +42,24 @@ public class JsonRendererTest {
             DataSample<Double> demand = new DataSample<Double>("source-d", "signature-d", new Double(0), null);
             ThermostatSignal ts = new ThermostatSignal(true, true, true, true, demand);
 
+            Marker m = new Marker("consume");
+
             tr.consume(new DataSample<ThermostatSignal>("source-ts", "signature-ts", ts, null));
+            
+            m.close();
 
             assertEquals("wrong queue size", 1, queue.size());
+            
+            ZoneSnapshot head = queue.peek();
+            
+            // Let's see how long it is in JSON
+            
+            Gson gson = new Gson();
+            String actual = gson.toJson(head);
 
-            logger.debug("queue head: " + queue.peek());
+            logger.debug("queue head (" + actual.length() + " bytes): " + actual);
 
-            assertEquals("wrong toString()", "json-thermostat: Cooling, CALLING, signal=0.0, current=0.0, setpoint=20.0, on hold", queue.peek().toString());
+            assertEquals("wrong toString()", "test-thermostat: Cooling, CALLING, signal=0.0, current=0.0, setpoint=20.0, on hold", queue.peek().toString());
 
         } finally {
             NDC.pop();
--- dz3-master/dz3-http/src/test/java/net/sf/dz3/view/http/v3/ProtobufRendererTest.java
+++ dz3-master/dz3-http/src/test/java/net/sf/dz3/view/http/v3/ProtobufRendererTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 import net.sf.dz3.device.model.ThermostatSignal;
 import net.sf.dz3.device.model.impl.ThermostatModel;
 import net.sf.dz3.device.sensor.impl.NullSensor;
+import net.sf.dz3.instrumentation.Marker;
 import net.sf.dz3.view.http.common.QueueFeeder;
 import net.sf.dz3.view.http.v3.ZoneSnapshotList.ZoneSnapshot;
 import net.sf.jukebox.datastream.signal.model.DataSample;
@@ -29,7 +30,7 @@ public class ProtobufRendererTest {
 
         try {
 
-            ThermostatModel t = new ThermostatModel("protobuf-thermostat", new NullSensor("null-address", 100), 20, 1, 0.000002,0, 2);
+            ThermostatModel t = new ThermostatModel("test-thermostat", new NullSensor("null-address", 100), 20, 1, 0.000002,0, 2);
             Map<String, Object> context = new HashMap<String, Object>();
             BlockingQueue<ZoneSnapshot> queue = new LinkedBlockingQueue<ZoneSnapshot>();
 
@@ -40,29 +41,34 @@ public class ProtobufRendererTest {
             long timestamp = System.currentTimeMillis();
             DataSample<Double> demand = new DataSample<Double>(timestamp, "source-d", "signature-d", new Double(0), null);
             ThermostatSignal ts = new ThermostatSignal(true, true, true, true, demand);
+            
+            Marker m = new Marker("consume");
 
             tr.consume(new DataSample<ThermostatSignal>(timestamp, "source-ts", "signature-ts", ts, null));
+            
+            m.close();
 
             assertEquals("wrong queue size", 1, queue.size());
             
-            String expected =
-                      "timestamp: " + timestamp + " "
-                    + "name: \"protobuf-thermostat\" "
-                    + "mode: COOLING "
-                    + "state: CALLING "
-                    + "signal: 0.0 "
-                    + "currentTemperature: 0.0 "
-                    + "setpointTemperature: 20.0 "
-                    + "enabled: true "
-                    + "onHold: true "
-                    + "voting: true "
-                    + "deviationSetpoint: 0.0 "
-                    + "deviationEnabled: false "
-                    + "deviationVoting: false ";
+            ZoneSnapshot head = queue.peek();
             
-            String actual = queue.peek().toString().replaceAll("\n", " ");
+            String actual = head.toString().replaceAll("\n", " ");
+            String expected =
+                    "timestamp: " + timestamp + " "
+                  + "name: \"test-thermostat\" "
+                  + "mode: COOLING "
+                  + "state: CALLING "
+                  + "signal: 0.0 "
+                  + "currentTemperature: 0.0 "
+                  + "setpointTemperature: 20.0 "
+                  + "enabled: true "
+                  + "onHold: true "
+                  + "voting: true "
+                  + "deviationSetpoint: 0.0 "
+                  + "deviationEnabled: false "
+                  + "deviationVoting: false ";
 
-            logger.debug("queue head: " + actual);
+            logger.debug("queue head (" + head.toByteString().size() + " bytes): " + actual);
             
             assertEquals("wrong toString()", expected, actual);
 
